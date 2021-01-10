96ed50fb4068503d79419a85a3a63a74951e82d8
Ayfri <pierre.ayfri@gmail.com>
2021-01-10 03:56:43 +0100

    Translated Registry componant.

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -300,7 +300,7 @@ function Registry(): React.ReactElement {
       <CookieBanner />
       <div className="bg-gray-50 min-h-full">
         <Header
-          subtitle={name === "std" ? "Standard Library" : "Third Party Modules"}
+          subtitle={name === "std" ? "Librairie Standard" : "Module Tier"}
           widerContent={true}
         />
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 py-2 pb-8 pt-4">
@@ -315,8 +315,8 @@ function Registry(): React.ReactElement {
               if (versions === null) {
                 return (
                   <ErrorMessage
-                    title="404 - Not Found"
-                    body="This module does not exist."
+                    title="404 - Non trouvé"
+                    body="Ce module n'existe pas."
                   />
                 );
               } else if (
@@ -325,10 +325,10 @@ function Registry(): React.ReactElement {
               ) {
                 return (
                   <ErrorMessage
-                    title="No uploaded versions"
-                    body={`This module name has been reserved for a repository, but no versions have been uploaded yet. Modules that do not upload a version within 30 days of registration will be removed. ${
+                    title="Aucune version publiée"
+                    body={`Ce nom de module a été réservé pour un dépôt, mais aucune version n'a encore été publiée. Les modules qui ne publient pas une version dans les 30 jours suivant leur enregistrement seront supprimés. ${
                       versions.isLegacy
-                        ? "If you are the owner of this module, please re-add the GitHub repository with deno.land/x (by following the instructions at https://deno.land/x#add), and publish a new version."
+                        ? "Si vous êtes le propriétaire de ce module, veuillez ré-ajouter le dépôt GitHub avec deno.land/x (en suivant les instructions sur https://deno.land/x#add), et publier une nouvelle version."
                         : ""
                     }`}
                   />
@@ -345,8 +345,8 @@ function Registry(): React.ReactElement {
                       ) {
                         return (
                           <ErrorMessage
-                            title="404 - Not Found"
-                            body="This version does not exist for this module."
+                            title="404 - Non trouvé"
+                            body="Cette version n'existe pas pour ce module."
                           />
                         );
                       } else if (
@@ -357,8 +357,8 @@ function Registry(): React.ReactElement {
                       ) {
                         return (
                           <ErrorMessage
-                            title="404 - Not Found"
-                            body="This file or directory could not be found."
+                            title="404 - Non trouvé"
+                            body="Ce fichier ou répertoire n'a pas pu être trouvé."
                           />
                         );
                       } else if (!dirEntries && typeof raw !== "string") {
@@ -435,7 +435,7 @@ function Registry(): React.ReactElement {
                                 fill="currentColor"
                                 viewBox="0 0 24 24"
                               >
-                                <title>GitHub Repository</title>
+                                <title>Dépôt GitHub</title>
                                 <path
                                   fillRule="evenodd"
                                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -450,7 +450,7 @@ function Registry(): React.ReactElement {
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                               >
-                                <title>GitHub Stars</title>
+                                <title>Étoiles de GitHub</title>
                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                               </svg>
                               <div className="w-1/6 sm:w-1/5 bg-gray-100 h-4"></div>
@@ -468,7 +468,7 @@ function Registry(): React.ReactElement {
                                 fill="currentColor"
                                 viewBox="0 0 24 24"
                               >
-                                <title>GitHub Repository</title>
+                                <title>Dépôt GitHub</title>
                                 <path
                                   fillRule="evenodd"
                                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -488,7 +488,7 @@ function Registry(): React.ReactElement {
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                               >
-                                <title>GitHub Stars</title>
+                                <title>Étoiles de GitHub</title>
                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                               </svg>
                               <div>{moduleMeta.star_count}</div>
@@ -506,7 +506,7 @@ function Registry(): React.ReactElement {
                     </div>
 
                     <div className="max-w-sm w-full shadow-sm rounded-lg border border-gray-200 p-4">
-                      <p className="text-md font-semibold mb-2">Version Info</p>
+                      <p className="text-md font-semibold mb-2">Informations sur la version</p>
                       {versionMeta === undefined ? (
                         <div className="mt-2 flex items-center py-0.5">
                           <svg
@@ -514,7 +514,7 @@ function Registry(): React.ReactElement {
                             viewBox="0 0 20 20"
                             fill="currentColor"
                           >
-                            <title>Tagged at</title>
+                            <title>Publié à</title>
                             <path
                               fillRule="evenodd"
                               d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
@@ -530,7 +530,7 @@ function Registry(): React.ReactElement {
                             viewBox="0 0 20 20"
                             fill="currentColor"
                           >
-                            <title>Tagged at</title>
+                            <title>Publié à</title>
                             <path
                               fillRule="evenodd"
                               d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
@@ -546,7 +546,7 @@ function Registry(): React.ReactElement {
                     {documentationURL && externalDependencies !== null ? (
                       <div className="max-w-sm w-full shadow-sm rounded-lg border border-gray-200 p-4">
                         <p className="text-md font-semibold mb-2">
-                          External Dependencies
+                          Dépendances externes
                         </p>
                         {externalDependencies === undefined ? (
                           <>
@@ -583,11 +583,11 @@ function Registry(): React.ReactElement {
                             </div>
                             <div className="text-sm mt-2 italic">
                               {externalDependencies.length === 0
-                                ? "No external dependencies 🎉"
+                                ? "Aucune dépendance extérieure 🎉"
                                 : externalDependencies.length +
                                   (externalDependencies.length === 1
-                                    ? " external dependency"
-                                    : " external dependencies")}
+                                    ? " dépendance extérieure"
+                                    : " dépendances extérieures")}
                             </div>
                           </>
                         )}
@@ -709,7 +709,7 @@ function VersionSelector({
           aria-label="Go to latest version"
           onClick={() => onChange(versions[0])}
         >
-          Go to latest
+          Aller à la dernière version
         </button>
       ) : null}
     </div>
